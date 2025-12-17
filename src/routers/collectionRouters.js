bc76d23faee4c31cf64ea937a486ec494ab15a8c
Hugo Pottier <pottier234@campus.unicaen.fr>
2025-12-17 14:40:39 +0100

    ajouter ownerid veritable

--- src/routers/collectionRouters.js
+++ src/routers/collectionRouters.js
@@ -2,11 +2,14 @@ import {Router} from 'express'
 import { collectionSchema } from '../models/collection.js'
 import { validateBody } from "../middleware/validation.js";
 import { created, read } from '../controllers/collectionController.js';
+import { authenticateToken } from '../middleware/authenticateToken.js';
 
 
 const router = Router()
 
-router.post('/create', validateBody(collectionSchema), created)
+router.use(authenticateToken)
+
+router.post('/', validateBody(collectionSchema), created)
 router.get('/', read)
 
 export default router
