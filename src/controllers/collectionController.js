80d4e173fa0be9b2b07a53ce83c787c70b76a78a
Martin <martinbazogepro@gmail.com>
2026-01-10 22:03:12 +0100

    Collections -> /:id & /mycollections ajoutés

--- src/controllers/collectionController.js
+++ src/controllers/collectionController.js
@@ -1,3 +1,4 @@
+import { eq } from "drizzle-orm"
 import { db } from '../db/database.js';
 import { collectionTable } from '../db/schema.js'
 
@@ -32,7 +33,7 @@ export const created = async(req, res) => {
 }
 
 
-export const read = async (req, res) => {
+export const getAllCollections = async (req, res) => {
     try {
         const [readCollection] =await db.select().from(collectionTable);
         res.status(200).json({
@@ -46,3 +47,58 @@ export const read = async (req, res) => {
         });
     }
 };
+
+export const getOneCollection = async (request, response) => {
+    const { id } = request.params
+    
+    try {
+        const [collection] = await db.select().from(collectionTable).where(eq(collectionTable.id, id))
+
+        if (!collection) {
+            return response.status(404).json({
+                error: "Collection non trouvée",
+            })
+        }
+
+        const userId = request.userId.userId
+
+        if ((!userId) || (userId != collection.ownerId && !collection.isPublic)) {
+            return response.status(403).json({
+                error: "Non autorisé",
+            })
+        }
+
+        response.status(200).json({
+            message: "Collection trouvée",
+            data: collection,
+        })
+    } catch (error) {
+        console.error(error)
+        response.status(500).json({
+            error: "Pas réussi à obtenir la collection",
+        })
+    }
+}
+
+export const getMyCollections = async (request, response) => {
+    try {
+        const userId = request.userId.userId
+        const [collection] = await db.select().from(collectionTable).where(eq(collectionTable.ownerId, userId))
+
+        if (!collection) {
+            return response.status(404).json({
+                error: "Collections non trouvées",
+            })
+        }
+
+        response.status(200).json({
+            message: "Collections trouvées",
+            data: collection,
+        })
+    } catch (error) {
+        console.error(error)
+        response.status(500).json({
+            error: "Pas réussi à obtenir la collection",
+        })
+    }
+}
--- src/routers/collectionRouters.js
+++ src/routers/collectionRouters.js
@@ -1,7 +1,7 @@
 import {Router} from 'express'
 import { collectionSchema } from '../models/collection.js'
-import { validateBody } from "../middleware/validation.js";
-import { created, read } from '../controllers/collectionController.js';
+import { validateBody, validateParams } from "../middleware/validation.js";
+import { created, getAllCollections, getOneCollection, getMyCollections } from '../controllers/collectionController.js';
 import { authenticateToken } from '../middleware/authenticateToken.js';
 
 
@@ -10,6 +10,8 @@ const router = Router()
 router.use(authenticateToken)
 
 router.post('/', validateBody(collectionSchema), created)
-router.get('/', read)
+router.get('/', getAllCollections)
+router.get("/mycollections", getMyCollections)
+router.get("/:id", validateParams(collectionSchema), getOneCollection)
 
 export default router
